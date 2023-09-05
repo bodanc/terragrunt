208f579270296724c0f9bedd2a78c8204abeb038
Bogdan Dancu <bogdan.dancu@threatfabric.com>
2023-09-05 14:40:49 +0200

    tf fmt --recursive

--- modules/main.tf
+++ modules/main.tf
@@ -14,23 +14,23 @@ module "vpc" {
   public_subnets  = ["172.30.2.0/24", "172.30.3.0/24"]
 
   map_public_ip_on_launch = true
-  enable_dns_hostnames = true
-  enable_nat_gateway   = true
-  single_nat_gateway   = true
+  enable_dns_hostnames    = true
+  enable_nat_gateway      = true
+  single_nat_gateway      = true
 
   tags = {
-    Terraform   = "true"
+    Terraform = "true"
   }
 }
 
 resource "aws_instance" "ec2_instance_0" {
-  subnet_id         = module.vpc.public_subnets[0]
-  ami               = var.ami
-  instance_type     = var.instance_type
+  subnet_id     = module.vpc.public_subnets[0]
+  ami           = var.ami
+  instance_type = var.instance_type
 }
 
 resource "aws_instance" "ec2_instance_1" {
-  subnet_id         = module.vpc.public_subnets[1]
-  ami               = var.ami
-  instance_type     = var.instance_type
+  subnet_id     = module.vpc.public_subnets[1]
+  ami           = var.ami
+  instance_type = var.instance_type
 }
