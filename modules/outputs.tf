05302668347656f25ba4b8710055180acdbe7a28
Bogdan Dancu <bogdan.dancu@threatfabric.com>
2023-09-05 14:39:17 +0200

    fixes outputs.tf ec2 instance names

--- modules/outputs.tf
+++ modules/outputs.tf
@@ -1,7 +1,7 @@
-output "public_ip_1" {
+output "public_ip_0" {
   value = aws_instance.ec2_instance_0.public_ip
 }
 
-output "public_ip_2" {
+output "public_ip_1" {
   value = aws_instance.ec2_instance_1.public_ip
 }
